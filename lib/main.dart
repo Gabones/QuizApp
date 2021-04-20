28894047b6d3328aee18b103d10d610f941787bc
Gabriel Albuquerque da Rosa <gabaoalb@gmail.com>
2021-04-19 23:05:26 -0300

    lesson 30

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,33 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<String> questions = [
+      'Qual sua cor favorita?',
+      'Qual seu animal preferido?'
+    ];
     return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Quiz App?'),
-          ),
-          body: Text('Default text!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz App?'),
         ),
+        body: Column(
+          children: [
+            Text(questions[0]),
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
