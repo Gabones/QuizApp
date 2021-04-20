74060c02835b385388ff1d0b07d41ee31aee4a6f
Gabriel Albuquerque da Rosa <gabaoalb@hotmail.com>
2021-04-20 13:24:02 -0300

    lesson 38

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,23 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
-    void answerQuestion() {
-      print('Answer chosen!');
-    }
-
     List<String> questions = [
       'Qual sua cor favorita?',
       'Qual seu animal preferido?'
@@ -24,18 +34,21 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text(questions[0]),
+            Text(questions[_questionIndex]),
             ElevatedButton(
               child: Text('Resposta 1'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             ElevatedButton(
               child: Text('Resposta 2'),
-              onPressed: answerQuestion,
+              onPressed: () => print('Answer 2 chosen!'),
             ),
             ElevatedButton(
               child: Text('Resposta 3'),
-              onPressed: answerQuestion,
+              onPressed: () {
+                //...
+                print('Answer 3 chosen!');
+              },
             )
           ],
         ),
